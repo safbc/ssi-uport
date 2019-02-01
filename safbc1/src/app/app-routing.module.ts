5afafc90a6e85d80bd41de3d2164085eeb896df9
Gary de Beer <garyd@Garys-MacBook-Pro.local>
2019-02-02 00:28:00 +0200

    First scaffold

---
+++ safbc1/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        loadChildren: './ui/home/home.module#HomePageModule'
+    },
+    {
+        path: 'list',
+        loadChildren: './ui/list/list.module#ListPageModule'
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
