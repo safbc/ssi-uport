02fba4a9b7be900dd321b0c5d4438c8a0cf4fd57
Gary De Beer <GaryD@BankservAfrica.com>
2019-02-12 18:33:10 +0200

    uport service will handle this

--- safbc-stand/src/app/ui/login/login.component.ts
+++ safbc-stand/src/app/ui/login/login.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// import * as JWT from 'did-jwt';
-// import { Credentials } from 'uport-credentials';
 
 @Component({
   selector: 'app-login',
@@ -8,12 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  // credentials = new Credentials({
-  //   appName: 'SAFBC Stand',
-  //   did: 'did:ethr:0x31486054a6ad2c0b685cd89ce0ba018e210d504e',
-  //   privateKey: 'ef6a01d0d98ba08bd23ee8b0c650076c65d629560940de9935d0f46f00679e01'
-  // });
 
   constructor(
 
