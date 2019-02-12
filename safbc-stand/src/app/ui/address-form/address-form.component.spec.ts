b3261c5f10f502dd4c971c4c6e05cbd805f37859
Gary De Beer <GaryD@BankservAfrica.com>
2019-02-12 17:38:08 +0200

    generated some pages

---
+++ safbc-stand/src/app/ui/address-form/address-form.component.spec.ts
@@ -0,0 +1,42 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatInputModule,
+  MatRadioModule,
+  MatSelectModule,
+} from '@angular/material';
+
+import { AddressFormComponent } from './address-form.component';
+
+describe('AddressFormComponent', () => {
+  let component: AddressFormComponent;
+  let fixture: ComponentFixture<AddressFormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ AddressFormComponent ],
+      imports: [
+        NoopAnimationsModule,
+        ReactiveFormsModule,
+        MatButtonModule,
+        MatCardModule,
+        MatInputModule,
+        MatRadioModule,
+        MatSelectModule,
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AddressFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should compile', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ safbc-stand/src/app/ui/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+// import * as JWT from 'did-jwt';
+// import { Credentials } from 'uport-credentials';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  // credentials = new Credentials({
+  //   appName: 'SAFBC Stand',
+  //   did: 'did:ethr:0x31486054a6ad2c0b685cd89ce0ba018e210d504e',
+  //   privateKey: 'ef6a01d0d98ba08bd23ee8b0c650076c65d629560940de9935d0f46f00679e01'
+  // });
+
+  constructor(
+
+  ) {
+
+  }
+
+  ngOnInit() {
+  }
+
+}
