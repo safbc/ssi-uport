202798f023c2fd97035a75fbe0ab293b554717b4
Gary De Beer <GaryD@BankservAfrica.com>
2019-02-12 17:39:17 +0200

    page navigation

--- safbc-stand/src/app/app-routing.module.ts
+++ safbc-stand/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddressFormComponent } from './ui/address-form/address-form.component';
+import { LoginComponent } from './ui/login/login.component';
+import { PageNotFoundComponent } from './ui/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'form', component: AddressFormComponent },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: [
+    // LoginComponent,
+    // PageNotFoundComponent
+  ]
 })
 export class AppRoutingModule { }
