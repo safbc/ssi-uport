a3f8a1582fbc7a58f31a2d1dde75c49981ae263a
Gary de Beer <garyd@Garys-MacBook-Pro.local>
2019-02-18 01:42:17 +0200

    fix

--- safbc/safbc.js
+++ safbc/safbc.js
@@ -26,7 +26,7 @@ function register() {
     //Ask the user for their address information
     //by using default disclosure behavior.
     uport.requestDisclosure({
-        requested: [],
+        requested: ['nothing'],
         verified: ['SAFBC'],
         notifications: true
     })
