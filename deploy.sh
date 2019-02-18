c926d948797d99c66821f30ec71e0283a87f7605
Gary de Beer <garyd@Garys-MacBook-Pro.local>
2019-02-18 21:43:16 +0200

    copy script

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+git pull
+cp -r safbc/* /var/www/safbc.myid.africa/html/
+cp -r valr/* /var/www/valr.myid.africa/html/
+cp -r oldmutual/* /var/www/oldmutual.myid.africa/html/
+cp -r academy/* /var/www/blockchainacademy.myid.africa/html/
