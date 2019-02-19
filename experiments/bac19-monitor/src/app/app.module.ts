4d3d8e9dfb865714332a39fdc82bf90bf9be16e5
Gary De Beer <GaryD@BankservAfrica.com>
2019-02-19 15:57:06 +0200

    make it prettier

--- experiments/bac19-monitor/src/app/app.module.ts
+++ experiments/bac19-monitor/src/app/app.module.ts
@@ -1,19 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment.prod';
 
 @NgModule({
-    declarations: [AppComponent],
-    entryComponents: [],
-    imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+    declarations: [
+        AppComponent
+    ],
+    entryComponents: [
+
+    ],
+    imports: [
+        BrowserModule,
+        AngularFireModule.initializeApp(environment.firebaseConfig),  // Add this
+        AngularFirestoreModule,
+        IonicModule.forRoot(),
+        AppRoutingModule
+    ],
     providers: [
         { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
     ],
-    bootstrap: [AppComponent]
+    bootstrap: [
+        AppComponent
+    ]
 })
 export class AppModule { }
