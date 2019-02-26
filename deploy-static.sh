5f46e73abbfcce6f1b87b45c27f18e77a0b2b9ed
Gary de Beer <garyd@bankservafrica.com>
2019-02-26 18:49:22 +0200

    renamed scripts

--- deploy-static.sh
+++
@@ -1,6 +0,0 @@
-git pull
-cp -r safbc/* /var/www/safbc.myid.africa/html/
-cp -r valr/* /var/www/valr.myid.africa/html/
-cp -r oldmutual/* /var/www/oldmutual.myid.africa/html/
-cp -r academy/* /var/www/blockchainacademy.myid.africa/html/
-cp -r experiments/bac19-monitor/www/* /var/www/ssi-monitor.myid.africa/html
