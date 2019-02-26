fa96187bdedb978ef25cd4cfda26ee2bc3f22f68
Gary de Beer <garyd@bankservafrica.com>
2019-02-26 19:01:22 +0200

    updated script and readme

--- deploy-all.sh
+++ deploy-all.sh
@@ -1,30 +1,18 @@
 git pull
 
-cd ~/ssi-uport/ionic-safbc/
-npm i
-ionic build --prod
-sudo rm -rf /var/www/safbc.myid.africa/html/
-sudo mkdir /var/www/safbc.myid.africa/html/
-sudo cp -r www/* /var/www/safbc.myid.africa/html/
+echo Deploy Blockchain Academy
+bash deploy-academy.sh
 
-cd ~/ssi-uport/ionic-om/
-npm i
-ionic build --prod
-sudo rm -rf /var/www/oldmutual.myid.africa/html/
-sudo mkdir /var/www/oldmutual.myid.africa/html/
-sudo cp -r www/* /var/www/oldmutual.myid.africa/html/
+echo Deploy Old Mutual
+bash deploy-om.sh
 
-cd ~/ssi-uport/ionic-valr/
-npm i
-ionic build --prod
-sudo rm -rf /var/www/valr.myid.africa/html/
-sudo mkdir /var/www/valr.myid.africa/html/
-sudo cp -r www/* /var/www/valr.myid.africa/html/
+echo Deploy SAFBC
+bash deploy-safbc.sh
 
-cd ~/ssi-uport/ionic-academy/
-npm i
-ionic build --prod
-sudo rm -rf /var/www/blockchainacademy.myid.africa/html/
-sudo mkdir /var/www/blockchainacademy.myid.africa/html/
-sudo cp -r www/* /var/www/blockchainacademy.myid.africa/html/
+echo Deploy VALR
+bash deploy-valr.sh
 
+echo Deploy Monitor
+bash deploy-monitor.sh
+
+echo All done.
