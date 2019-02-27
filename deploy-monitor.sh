0bac26a0d1409850c3fb1bc5777d47ab55e8fe89
DibbsZA <dibbs.za@gmail.com>
2019-02-27 02:08:04 +0200

    fix monitor deploy script

--- deploy-monitor.sh
+++ deploy-monitor.sh
@@ -9,4 +9,4 @@ sudo rm -rf /var/www/ssi-monitor.myid.africa/html/
 sudo mkdir /var/www/ssi-monitor.myid.africa/html/
 
 echo Copying new files
-sudo cp -r www/* /var/ssi-www/monitor.myid.africa/html/
+sudo cp -r www/* /var/www/ssi-monitor.myid.africa/html/
